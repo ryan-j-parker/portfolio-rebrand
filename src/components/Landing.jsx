3546c829ac658f441e3a2c08ff0872bcfb71a591
Ryan J. Parker <ryan.jos.parker@gmail.com>
2023-05-24 23:43:18 -0700

    add monogram svg

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,90 +1,12 @@
 import React from "react";
 import "./Landing.css";
+import logo from '../rjp.svg';
 
 export default function Landing() {
   return (
     <div className="landing">
-      <div className="bg-box"></div>
-      <div className="box box1">
-        Ryan J. Parker <br />
-        <span className="box2">Creative Frontend Developer</span>
-      </div>
-      <div className="animated">Div</div>
-      <div className="animated">
-        {/* <ruby>
-          漢 <rp>(</rp>
-          <rt>Kan</rt>
-          <rp>)</rp>字 <rp>(</rp>
-          <rt>ji</rt>
-          <rp>)</rp>
-        </ruby> */}
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 518.22 288.74">
-          <g id="Rounded_Rectangle" data-name="Rounded Rectangle">
-            <rect width="512" height="256" rx="37.89" ry="37.89" />
-          </g>
-          <g id="Text">
-            <text className="cls-4" transform="translate(23.59 84.58)">
-              <tspan x="0" y="0">
-                NO
-              </tspan>
-              <tspan className="cls-7" x="113.54" y="0">
-                R
-              </tspan>
-              <tspan x="161.65" y="0">
-                THWE
-              </tspan>
-              <tspan className="cls-5" x="375.51" y="0">
-                S
-              </tspan>
-              <tspan x="420.58" y="0">
-                T
-              </tspan>
-            </text>
-            <text />
-            <text className="cls-1" transform="translate(19.5 217.62)">
-              <tspan x="0" y="0">
-                j222222k
-              </tspan>
-            </text>
-            <text className="cls-2" transform="translate(70.41 153.92)">
-              <tspan x="0" y="0">
-                DE
-              </tspan>
-              <tspan className="cls-9" x="97.46" y="0">
-                T
-              </tspan>
-              <tspan x="132.85" y="0">
-                AILING
-              </tspan>
-            </text>
-            <text className="cls-3" transform="translate(122.14 206.59)">
-              <tspan x="0" y="0">
-                MOBILE B
-              </tspan>
-              <tspan className="cls-8" x="92.64" y="0">
-                O
-              </tspan>
-              <tspan className="cls-6" x="106.34" y="0">
-                A
-              </tspan>
-              <tspan x="118.41" y="0">
-                T &amp;{" "}
-              </tspan>
-              <tspan className="cls-10" x="155.81" y="0">
-                R
-              </tspan>
-              <tspan x="169.74" y="0">
-                V SE
-              </tspan>
-              <tspan className="cls-10" x="211.5" y="0">
-                R
-              </tspan>
-              <tspan x="225.42" y="0">
-                VICE
-              </tspan>
-            </text>
-          </g>
-        </svg>
+      <div className="bg-box">
+            <img src={logo} />
       </div>
     </div>
   );
